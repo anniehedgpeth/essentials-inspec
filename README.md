2b9335ae45ca6c25770d1a73403c31b275d7e414
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-03-19 17:29:57 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# InSpec Profile
+## Chef Essentials for Windows Azure Environment 
+
+This InSpec profile validates the configuration of environments in Azure that will be used by those taking Chef Essentials Training given by 10th Magnitude.
---
+++ controls/essentials.rb
@@ -0,0 +1,51 @@
+# encoding: utf-8
+# copyright: 2015, The Authors
+# license: All rights reserved
+
+title 'Essentials for Windows Training Environments'
+
+# you can also use plain tests
+describe file('C:/Users/chef/credentials') do
+  it { should be_file }
+  it { should exist }
+end
+
+describe command('choco') do
+  it { should exist }
+end
+
+describe command('chef') do
+  it { should exist }
+end
+
+describe command('kitchen') do
+  it { should exist }
+end
+
+describe command('git') do
+  it { should exist }
+end
+
+describe command('chef-client --version') do
+  its('stdout') { should match (/Chef:/) }
+end
+
+describe service('visualstudiocode') do
+  it { should be_installed }
+end
+
+describe package('chrome') do
+  it { should be_installed }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe port(443) do
+  it { should be_listening }
+end
+
+describe port(5985) do
+  it { should be_listening }
+end
