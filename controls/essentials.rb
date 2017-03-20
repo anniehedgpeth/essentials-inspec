0ebdf509a003e4de48e955b4b4e57e34f1cd0c77
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-03-19 23:08:33 -0500

    commented out azure ports

--- controls/essentials.rb
+++ controls/essentials.rb
@@ -34,13 +34,13 @@ describe command('chef-client --version') do
   its('stdout') { should match (/Chef:/) }
 end
 
-describe port(80) do
-  it { should be_listening }
-end
+# describe port(80) do
+#   it { should be_listening }
+# end
 
-describe port(443) do
-  it { should be_listening }
-end
+# describe port(443) do
+#   it { should be_listening }
+# end
 
 describe port(5985) do
   it { should be_listening }
